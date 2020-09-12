81f976c501151eaf3dd23650ae2cf0b2bffdf8f0
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-09-12 13:43:21 +0200

    Implement begin and commit transaction in a single request

--- src/eneo4j_worker.erl
+++ src/eneo4j_worker.erl
@@ -6,25 +6,36 @@
 
 -type start_error() :: {already_started, pid()} | term().
 -type start_result() :: {ok, pid()} | ignore | {error, start_error()}.
--type url() :: bitstring().
+-type url() :: string().
 -type header() :: {Key :: bitstring(), Value :: bitstring()}.
 -type headers() :: [header()].
 
 -type eneo4j_worker_config() :: #{
     url := url(),
+    db := string(),
     user => string(),
     password => string()
 }.
 
 -type eneo4j_worker_state() :: #{
     url := url(),
+    db := string(),
     headers := headers(),
     user => string(),
     password => string()
 }.
 
+-type cypher_query() :: string().
+-type query_params() :: #{string() => any()}.
+-type statement() :: #{statement := cypher_query(), parameters := query_params()}.
+-type statements() :: [statement()].
+-type eneo4j_call() :: discovery_api | {begin_and_commit_transaction, statements()}.
+
+-type response() :: {ok, pos_integer(), map()} | {error, Reason :: any()}.
+
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-export([build_statement/2]).
 
 -spec start_link(eneo4j_worker_config()) -> start_result().
 start_link(Args) ->
@@ -38,8 +49,15 @@ init(State = #{}) ->
     Headers = build_content_headers(),
     {ok, State#{headers => Headers}}.
 
+-spec handle_call(eneo4j_call(), From :: term(), eneo4j_worker_state()) ->
+    {reply, response(), eneo4j_worker_state()}.
 handle_call(discovery_api, _From, State = #{url := Url, headers := Headers}) ->
-    Response = hackney:request(get, Url, Headers, <<"">>, []),
+    Response = send_request_and_process_response(get, Url, Headers, <<"">>),
+    {reply, Response, State};
+handle_call({begin_and_commit_transaction, Statements}, _From, State = #{headers := Headers}) ->
+    Request = build_request(Statements),
+    FullUrl = build_url(begin_and_commit_transaction, State),
+    Response = send_request_and_process_response(post, FullUrl, Headers, Request),
     {reply, Response, State}.
 
 handle_cast(_Msg, State) ->
@@ -50,6 +68,13 @@ handle_info(_Info, State) ->
 
 terminate(_Reason, _State) ->
     ok.
+
+-spec build_statement(cypher_query(), query_params()) -> statement().
+build_statement(Query, Params) ->
+    #{
+        statement => Query,
+        parameters => Params
+    }.
 
 % Private functions
 
@@ -69,3 +94,20 @@ build_auth_header(User, Password) ->
     Basic = <<"Basic ">>,
     Val = base64:encode(User ++ ":" ++ Password),
     {<<"Authorization">>, <<Basic/binary, Val/binary>>}.
+
+build_request(Statements) ->
+    RawRequest = #{statements => Statements},
+    jiffy:encode(RawRequest).
+
+build_url(begin_and_commit_transaction, #{url := Url, db := DB}) ->
+    Url ++ "/db/" ++ DB ++ "/tx/commit".
+
+send_request_and_process_response(Method, Url, Headers, ReqBody) ->
+    case hackney:request(Method, Url, Headers, ReqBody, []) of
+        {ok, StatusCode, _RespHeaders, ClientRef} ->
+            {ok, ResponseBody} = hackney:body(ClientRef),
+            DecodedBody = jiffy:decode(ResponseBody, [return_maps]),
+            {ok, StatusCode, DecodedBody};
+        Response = {error, _Reason} ->
+            Response
+    end.
--- test/eneo4j_worker_SUITE.erl
+++ test/eneo4j_worker_SUITE.erl
@@ -8,6 +8,7 @@
 -define(AuthNeo4jUrl, "http://localhost:7474").
 -define(NoAuthNeo4jUrl, "http://localhost:7470").
 -define(Neo4jUser, "neo4j").
+-define(Neo4jDB, "neo4j").
 -define(Neo4jPassword, "test").
 
 init_per_suite(Config) ->
@@ -19,20 +20,48 @@ end_per_suite(_Config) ->
     ok.
 
 init_per_group(no_authentication, Config) ->
-    [{worker_config, #{url => ?NoAuthNeo4jUrl}} | Config];
+    WorkerConfig = #{url => ?NoAuthNeo4jUrl, db => ?Neo4jDB},
+    {ok, Worker} = eneo4j_worker:start_link(WorkerConfig),
+    Statement = eneo4j_worker:build_statement(
+        <<"CREATE (n:Person { name: 'Andy' }) RETURN n.name">>,
+        #{}
+    ),
+    gen_server:call(Worker, {begin_and_commit_transaction, [Statement]}),
+    [{worker, Worker}, {worker_config, WorkerConfig} | Config];
 init_per_group(with_authentication, Config) ->
-    [
-        {worker_config, #{url => ?AuthNeo4jUrl, user => ?Neo4jUser, password => ?Neo4jPassword}}
-        | Config
-    ].
+    WorkerConfig = #{
+        url => ?AuthNeo4jUrl,
+        user => ?Neo4jUser,
+        password => ?Neo4jPassword,
+        db => ?Neo4jDB
+    },
+    {ok, Worker} = eneo4j_worker:start_link(WorkerConfig),
+    Statement = eneo4j_worker:build_statement(
+        <<"CREATE (n:Person { name: 'Andy' }) RETURN n.name">>,
+        #{}
+    ),
+    gen_server:call(Worker, {begin_and_commit_transaction, [Statement]}),
+    [{worker, Worker}, {worker_config, WorkerConfig} | Config].
 
-end_per_group(_GroupName, _Config) ->
+end_per_group(_GroupName, Config) ->
+    WorkerConfig = proplists:get_value(worker_config, Config),
+    {ok, Worker} = eneo4j_worker:start_link(WorkerConfig),
+    Statement = eneo4j_worker:build_statement(<<"MATCH (n) DETACH DELETE n">>, #{}),
+    gen_server:call(Worker, {begin_and_commit_transaction, [Statement]}),
     ok.
 
 init_per_testcase(when_discovering_not_existing_address_econnrefused_is_returned, Config) ->
     DefaultWorkerConfig = proplists:get_value(worker_config, Config),
     WorkerConfig = DefaultWorkerConfig#{url => "http://localhost:7475"},
     [{worker_config, WorkerConfig} | Config];
+init_per_testcase(when_adding_node_transaction_is_successfully_committed_without_params, Config) ->
+    Query = <<"MATCH (n) WHERE n.name = 'Andy' RETURN n">>,
+    Params = #{},
+    [{cypher_query, Query}, {Params} | Config];
+init_per_testcase(when_adding_node_transaction_is_successfully_committed_with_params, Config) ->
+    Query = <<"MATCH (n) WHERE n.name = $name RETURN n">>,
+    Params = #{<<"name">> => <<"Andy">>},
+    [{cypher_query, Query}, {Params} | Config];
 init_per_testcase(_Case, Config) ->
     Config.
 
@@ -54,7 +83,9 @@ all() ->
 common_test_cases() ->
     [
         when_discovering_not_existing_address_econnrefused_is_returned,
-        when_discovering_existing_address_version_and_edition_are_returned
+        when_discovering_existing_address_version_and_edition_are_returned,
+        when_adding_node_transaction_is_successfully_committed_with_params,
+        when_adding_node_transaction_is_successfully_committed_without_params
     ].
 
 %%--------------------------------------------------------------------
@@ -74,9 +105,27 @@ when_discovering_existing_address_version_and_edition_are_returned(Config) ->
     WorkerConfig = proplists:get_value(worker_config, Config),
     {ok, Pid} = eneo4j_worker:start_link(WorkerConfig),
     ?_assertMatch(
-        #{
+        {ok, _, #{
             <<"neo4j_edition">> := <<"community">>,
             <<"neo4j_version">> := <<"4.1.1">>
-        },
+        }},
         gen_server:call(Pid, discovery_api)
+    ).
+
+% Begin and commit transaction
+when_adding_node_transaction_is_successfully_committed_without_params(Config) ->
+    when_adding_node_transaction_is_successfully_committed(Config).
+
+when_adding_node_transaction_is_successfully_committed_with_params(Config) ->
+    when_adding_node_transaction_is_successfully_committed(Config).
+
+when_adding_node_transaction_is_successfully_committed(Config) ->
+    Worker = proplists:get_value(worker, Config),
+    Query = proplists:get_value(cypher_query, Config),
+    Params = proplists:get_value(query_params, Config),
+
+    Statement = eneo4j_worker:build_statement(Query, Params),
+    ?_assertMatch(
+        {ok, _, #{}},
+        gen_server:call(Worker, {begin_and_commit_transaction, [Statement]})
     ).
