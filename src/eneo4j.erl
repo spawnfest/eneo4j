d4b93c9fec47164713109189b16cd05aa293333b
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-09-13 12:00:12 +0200

    Provide more conviniet reposne handling

--- src/eneo4j.erl
+++ src/eneo4j.erl
@@ -5,7 +5,8 @@
 -export([build_statement/2, build_statement/3]).
 
 discvery_api() ->
-    wpool:call(eneo4j_workers_pool, discovery_api, available_worker).
+    {ok, 200, Response} = wpool:call(eneo4j_workers_pool, discovery_api, available_worker),
+    Response.
 
 build_statement(Query, Params, IncludeStats) ->
     eneo4j_worker:build_statement(Query, Params, IncludeStats).
@@ -14,4 +15,12 @@ build_statement(Query, Params) ->
     eneo4j_worker:build_statement(Query, Params).
 
 begin_and_commit_transaction(Statements) ->
-    wpool:call(eneo4j_workers_pool, {begin_and_commit_transaction, Statements}, available_worker).
+    {ok, 200, Response} = wpool:call(
+        eneo4j_workers_pool,
+        {begin_and_commit_transaction, Statements},
+        available_worker
+    ),
+    case eneo4j_reponse:is_successful(Response) of
+        true -> Response;
+        Error -> Error
+    end.
